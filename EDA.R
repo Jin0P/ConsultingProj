7d1710342f8b1fa3a076c311390bd7840a42339c
Jinyoung Park <jpark50@luc.edu>
2022-10-08 12:12:11 -0500

    added DaysTKAMUA, D_PrePostROM, D_135PreROM

--- EDA.R
+++ EDA.R
@@ -1,12 +1,36 @@
 library(readxl)
 library(ggplot2)
 library(tidyverse)
-EDA <- read_excel("DeID2.xlsx", sheet = "2TKA_w.syn", range = cell_rows(1:34))
 
+#####################################################################3 
+# Data Setup
+#####################################################################3 
+
+EDA <- read_excel("DeID2.xlsx", sheet = "2TKA_w.syn", range = cell_rows(1:34))
 EDA <- EDA %>% mutate(MUA_type = ifelse(MUA==1&C_MUA!=1, 
   "MUA",ifelse(MUA!=1&C_MUA==1, "C_MUA","Both")))
-
 EDA$MUA_type <- factor(EDA$MUA_type, levels = c("MUA","C_MUA","Both"))
+
+EDA$race <- ifelse(EDA$race == "American Indian", "Other",EDA$race)
+
+names(EDA)[8] <- c("BMI")
+names(EDA)[11] <- c("Insurance")
+names(EDA)[15] <- c("StayDays")
+names(EDA)[17] <- c("ASA")
+EDA$ASA <- as.factor(EDA$ASA)
+
+names(EDA)[18] <- c("OperationT")
+names(EDA)[51] <- c("VarusValgus1")
+names(EDA)[64] <- c("VarusValgus2")
+EDA$VarusValgus1 <- as.factor(EDA$VarusValgus1)
+EDA$VarusValgus2 <- as.factor(EDA$VarusValgus2)
+
+EDA<- EDA %>% mutate(Days2TKA= as.integer(difftime(`Date of contralateral TKA`,surgery_date, units = "days")))
+names(EDA)[56] <- c("DateMUA")
+EDA$DateMUA <- ifelse(EDA$DateMUA == "other knee", NA,EDA$DateMUA)
+EDA$DateMUA<- as.Date(as.integer(EDA$DateMUA),origin= "1899-12-30")
+EDA<- EDA %>% mutate(DaysTKAMUA= difftime(DateMUA,surgery_date, units = "days"))
+
 
 
 # overall summary
@@ -30,7 +54,6 @@ ggplot(data = EDA) +
   coord_flip()
 
 # race
-EDA$race <- ifelse(EDA$race == "American Indian", "Other",EDA$race)
 ggplot(data = EDA) +
   geom_bar(mapping = aes(x = race, fill=race))+
   facet_wrap(MUA_type~., nrow =2)
@@ -42,7 +65,6 @@ ggplot(data = EDA) +
 
 # BMI (https://www.cdc.gov/healthyweight/assessing/index.html#:~:text=If%20your%20BMI%20is%20less,falls%20within%20the%20obese%20range.)
 # if your BMI is higher, obese range. If your BMI is 25.0 to 29.9, it falls within the overweight range.
-names(EDA)[8] <- c("BMI")
 ggplot(data = EDA) +
   geom_boxplot(mapping = aes(x = BMI, y = MUA_type, color=MUA_type))+
   coord_flip()
@@ -53,13 +75,11 @@ ggplot(data = EDA) +
   facet_wrap(MUA_type~., nrow =2)
 
 # financial_class (insurance type) ??? 
-names(EDA)[11] <- c("Insurance")
 ggplot(data = EDA) +
   geom_bar(mapping = aes(x = Insurance, fill=Insurance))+
   facet_wrap(MUA_type~., nrow =2)
 
 # " Length of stay (days)" 
-names(EDA)[15] <- c("StayDays")
 ggplot(data = EDA) +
   geom_point(mapping = aes(x = StayDays, y = MUA_type, color=MUA_type))+
   coord_flip()
@@ -68,9 +88,6 @@ ggplot(data = EDA,mapping = aes(x = StayDays, y = MUA_type, color=MUA_type)) +
   geom_point() +   coord_flip()
 
 # ASA
-names(EDA)[17] <- c("ASA")
-EDA$ASA <- as.factor(EDA$ASA)
-
 ggplot(data = EDA) +
   geom_bar(mapping = aes(x = MUA_type, fill=ASA), position = "dodge")
 
@@ -80,7 +97,6 @@ ggplot(data = EDA) +
 #  facet_grid(.~MUA_type)
 
 # operation time
-names(EDA)[18] <- c("OperationT")
 ggplot(data = EDA,mapping = aes(x =OperationT, y = MUA_type, color=MUA_type)) +
 geom_point() +   coord_flip() + 
   labs(x = "Operation time", y = NULL, color = NULL)
@@ -88,15 +104,13 @@ geom_point() +   coord_flip() +
 # categorical PCA? 
 apply(EDA[,c(20:41)],2,sum) # using BMI more make sense.. 
 
-
 # Jason code 
 library(ggcorrplot)
 comorb_2 = EDA[,c(20, 24:38)] 
 ggcorrplot(cor(comorb_2), title = "Correlation matrix of comorbities of patients who underwent MUA")
 
-
 # The # days between 2 TKAs 
-EDA<- EDA %>% mutate(Days2TKA= as.integer(difftime(`Date of contralateral TKA`,surgery_date, units = "days")))
+#EDA<- EDA %>% mutate(Days2TKA= as.integer(difftime(`Date of contralateral TKA`,surgery_date, units = "days")))
 
 ggplot(data = EDA,mapping = aes(x =Days2TKA, y = MUA_type, color=MUA_type)) +
   geom_point() +   coord_flip() + 
@@ -107,17 +121,41 @@ ggplot(data = EDA,mapping = aes(x =Days2TKA, y = MUA_type, color=MUA_type)) +
   labs(x = "Days2TKA", y = NULL, color = NULL)
 
 # varus/valgus ((normal=0, varus=1, valgus=2))
-names(EDA)[51] <- c("VarusValgus1")
-names(EDA)[64] <- c("VarusValgus2")
-
-EDA$VarusValgus1 <- as.factor(EDA$VarusValgus1)
-EDA$VarusValgus2 <- as.factor(EDA$VarusValgus2)
 
 ggplot(data = EDA) +
   geom_bar(mapping = aes(x = MUA_type, fill=VarusValgus1), position = "dodge") + 
   labs(x = NULL, y = NULL, fill = NULL, title = "Varus/Valgus (normal=0, varus=1, valgus=2)" )
 
 # The # days between TKA & MUA  
-EDA<- EDA %>% mutate(DaysTKAMUA= as.integer(difftime(`Date of MUA`,surgery_date, units = "days")))
 
+#EDA$DateMUA <- ifelse(EDA$DateMUA == "other knee", NA,EDA$DateMUA)
+#EDA$DateMUA<- as.Date(as.integer(EDA$DateMUA),origin= "1899-12-30")
+#EDA<- EDA %>% mutate(DaysTKAMUA= difftime(DateMUA,surgery_date, units = "days"))
+
+ggplot(data = EDA,mapping = aes(x =DaysTKAMUA, y = MUA_type, color=MUA_type)) +
+  geom_point() +   coord_flip() + 
+  labs(x = "the # days between TKA and MUA", y = NULL, color = NULL)
+
+
+# The difference between pre-op ROM and post-op ROM (1)
+names(EDA)
+names(EDA)[c(49,52,54)] <- c("PreROM1", "PostROM11","PostROM12")
+EDA <- EDA %>% mutate(D_PrePostROM= PostROM11-PreROM1)
+EDA$MUA <- as.factor(EDA$MUA)
+EDA$C_MUA <- as.factor(EDA$C_MUA)
+
+ggplot(data = EDA,mapping = aes(x =D_PrePostROM, y = MUA, color=MUA)) +
+  geom_point() + coord_flip() + 
+  labs(x = "Post ROM - Pre ROM", y = "0: No MUA, 1: MUA", color = NULL)
+
+# The difference betweeen 135 - pre-op ROM
+EDA<-EDA %>% mutate(D_135preROM= 135-PreROM1)
+
+ggplot(data = EDA,mapping = aes(x =D_135preROM, y = MUA, color=MUA)) +
+  geom_point() + coord_flip() + 
+  labs(x = "135 - Pre ROM", y = "0: No MUA, 1: MUA", color = NULL)
+
+ggplot(data = EDA,mapping = aes(x =D_135preROM, y = C_MUA, color=C_MUA)) +
+  geom_point() + coord_flip() + 
+  labs(x = "135 - Pre ROM", y = "0: No C_MUA, 1: C_MUA", color = NULL)
 
